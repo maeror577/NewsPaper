85ca4abb672d48ecd316702850240d545732305e
rippedpleasure <maeror577@gmail.com>
2021-03-09 20:58:58 +0500

    Add search functionality and post creating, editing and deleting

--- news/views.py
+++ news/views.py
@@ -1,14 +1,55 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import (ListView, DetailView, UpdateView,
+                                  CreateView, DeleteView)
+
 from .models import Post
+from .filters import NewsFilter
+from .forms import NewsForm
 
 
 class NewsList(ListView):
     model = Post
     template_name = 'news.html'
     context_object_name = 'news'
+    ordering = ['-posted']
+    paginate_by = 10
 
 
-class PostDetail(DetailView):
+class PostDetailView(DetailView):
+    template_name = 'post_detail.html'
+    queryset = Post.objects.all()
+
+
+class PostCreateView(CreateView):
+    template_name = 'post_create.html'
+    form_class = NewsForm
+    success_url = '/news'
+
+
+class PostEditView(UpdateView):
+    template_name = 'post_create.html'
+    form_class = NewsForm
+    success_url = '/news'
+
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
+
+
+class PostDeleteView(DeleteView):
+    template_name = 'post_delete.html'
+    queryset = Post.objects.all()
+    success_url = '/news'
+
+
+class Search(ListView):
     model = Post
-    template_name = 'post.html'
-    context_object_name = 'post'
+    template_name = 'search.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = NewsFilter(
+            self.request.GET,
+            queryset=self.get_queryset()
+        )
+        return context
